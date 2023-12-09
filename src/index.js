9e4960423189c096455589e95e9e7476db5c3145
Shubham Kumar <shubham9905374230@gmail.com>
2023-12-10 01:22:52 +0530

    publish branch

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<Auth0Provider
+    domain="dev-vzdnd7znxxrglh86.us.auth0.com"
+    clientId="4WqSc5cPjW1zJnhgiWAVzIKCKlQYtSeL"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
